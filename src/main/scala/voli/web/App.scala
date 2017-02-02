036bab930c7da5bf4fa1427f79915d64faf1c172
vadim <vadim.oliinyk@gmail.com>
2017-02-01 19:23:30 -0700

    Refactored configuration

--- src/main/scala/voli/web/App.scala
+++ src/main/scala/voli/web/App.scala
@@ -5,7 +5,7 @@ import java.io.RandomAccessFile
 import com.google.gson.Gson
 import ro.pippo.core.Application
 import ro.pippo.core.route.RouteContext
-import voli.{Crawler, config}
+import voli.index._
 
 import scala.collection.JavaConverters._
 
@@ -17,10 +17,10 @@ class App extends  Application {
     GET("/", (ctx: RouteContext) => ctx.render("main.mustache"))
     GET("/api/search", (ctx: RouteContext) => {
 
-//      val pointer: Long = Crawler.index0.dictionary.getOrElse(ctx.getRequest.getQueryParameter("q").toString, 0)
-//      val indexFile = new RandomAccessFile(config.indexFile.toFile, "r")
-//      indexFile.seek(pointer)
-//      val line = new Index(1).Line(indexFile.readLine())
+      val pointer: Long = Crawler.index0.dictionary.getOrElse(ctx.getRequest.getQueryParameter("q").toString, 0)
+      val indexFile = new RandomAccessFile(voli.index.systemConfig.indexFilePath.toFile, "r")
+      indexFile.seek(pointer)
+      val line = Line(indexFile.readLine())
       val gson = new Gson()
       val o1 = Map("title" -> "test1", "description" -> "desc1").asJava
       val o2 = Map("title" -> "test2", "description" -> "desc2").asJava
