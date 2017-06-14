5b4d2e563dedde46c1e102c7ffc0f4b7b9303932
vadym.oliinyk <vadym.oliinyk@wgu.edu>
2017-06-14 14:22:37 -0600

    Directory listing for testing purposes

--- src/main/scala/voli/web/App.scala
+++ src/main/scala/voli/web/App.scala
@@ -2,7 +2,7 @@ package voli.web
 
 import com.google.gson.Gson
 import ro.pippo.core.Application
-import ro.pippo.core.route.RouteContext
+import ro.pippo.core.route.{DirectoryHandler, RouteContext}
 import voli.index.IndexUtility
 
 import scala.collection.JavaConverters._
@@ -13,11 +13,13 @@ class App extends  Application {
   override def onInit(): Unit = {
     addPublicResourceRoute()
     addWebjarsResourceRoute()
+    val directoryHandler = new DirectoryHandler("src", "/")
 
     GET("/", (ctx: RouteContext) => ctx.render("main.ftl"))
     GET("/api/search", (ctx: RouteContext) => {
       val results = IndexUtility.search(ctx.getRequest.getQueryParameter("q").toString)
       ctx.json().send(gson.toJson(results.asJava))
     })
+    GET(directoryHandler.getUriPattern, directoryHandler)
   }
 }
